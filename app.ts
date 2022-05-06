c760c80573c250ffc12f43c5af396a0fe83b3a8a
Nike <nkaz2003@gmail.com>
2022-05-06 15:18:38 +0300

    Optionals and request from server

--- app.ts
+++ app.ts
@@ -1,17 +1,113 @@
 interface User {
-	name: string;
-}
-
-// дополнение
-interface User {
-	age: number;
+	login: string;
+	password?: string;
 }
 
 const user: User = {
-	name: 'Nike',
-	age: 18,
+	login: 'my_login',
 };
 
-/////////////////
+function multiply(a: number, b?: number): number {
+	if (b) return a * b;
+	else return a * a;
+}
 
-type ID = string | number;
+interface UserPro {
+	login: string;
+	password?: {
+		type: 'primary' | 'secondary';
+	};
+}
+
+function testPass(user: UserPro) {
+	const type = user.password?.type;
+	// const type = user.password!.type; // если мы точно уверены, что password точно есть
+	console.log(type);
+}
+
+testPass({ login: 'my_login', password: { type: 'primary' } });
+
+function test(param?: string): string | number {
+	return param ?? multiply(5);
+}
+
+// Упражнение
+
+// Запрос
+// {
+// 	"sum": 10000,
+// 	"from": 2,
+// 	"to": 4
+// }
+
+// Возможные ответы
+// {
+// 	"status": "success",
+// 	"data": {
+// 		"databaseId": 567,
+// 		"sum": 10000,
+// 		"from": 2,
+// 		"to": 4
+// 	}
+// }
+// {
+// 	"status": "failed",
+// 	"data": {
+// 		"errorMessage": "Недостаточно средств",
+// 		"errorCode": 4
+// 	}
+// }
+
+interface IPayment {
+	sum: number;
+	from: number;
+	to: number;
+}
+
+interface IPaymentRequest extends IPayment {}
+
+interface IErrorData {
+	errorMessage: string;
+	errorCode: number;
+}
+
+interface ISuccessData extends IPayment {
+	databaseId: number;
+}
+
+interface IResponseSuccess {
+	status: 'success';
+	data: ISuccessData;
+}
+
+interface IResponseError {
+	status: 'failed';
+	data: IErrorData;
+}
+
+function request(req: IPaymentRequest): IResponseSuccess | IResponseError {
+	if (req.sum >= 5000)
+		return {
+			status: 'success',
+			data: {
+				databaseId: 567,
+				...req,
+			},
+		};
+
+	return {
+		status: 'failed',
+		data: {
+			errorMessage: 'Недостаточно средств',
+			errorCode: 4,
+		},
+	};
+}
+
+const data: IPaymentRequest = {
+	sum: 5000,
+	from: 1,
+	to: 2,
+};
+
+console.log(request(data));
