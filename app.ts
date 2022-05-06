dc6e019a323ca1bd100df78fcee87857d6687c45
Nike <nkaz2003@gmail.com>
2022-05-06 14:33:06 +0300

    Difference between types and interfaces

--- app.ts
+++ app.ts
@@ -1,33 +1,17 @@
 interface User {
 	name: string;
+}
+
+// дополнение
+interface User {
 	age: number;
-	skills: string[];
-
-	log: (id: number) => string;
 }
 
-interface Role {
-	role: string;
-}
-
-interface UserWithRole extends User, Role {
-	roleId: number;
-}
-
-let user: UserWithRole = {
+const user: User = {
 	name: 'Nike',
 	age: 18,
-	skills: ['1', '2'],
-	roleId: 1,
-	role: 'smth',
-
-	log(id) {
-		return '';
-	},
 };
 
-interface UserDict {
-	[key: number]: User; // сколько угодно свойств типа (key: number; value: User)
-}
+/////////////////
 
-type ud = Record<number, User>; // то же самое
+type ID = string | number;
