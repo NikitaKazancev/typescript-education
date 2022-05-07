fd4b59bbee1ce6b152b56c0886f47c46310b72f4
Nike <nkaz2003@gmail.com>
2022-05-07 18:13:42 +0300

    Implements

--- app.ts
+++ app.ts
@@ -1,12 +1,30 @@
-class User {
-	skills: string[];
+interface ILogger {
+	log(...args: any[]): void;
+	error(...args: any[]): void;
+}
 
-	addSkill(skill: string): void;
-	addSkill(skills: string[]): void;
-	addSkill(data: string | string[]): void {
-		if (typeof data === 'string') this.skills.push(data);
-		else this.skills.concat(data);
+class Logger implements ILogger {
+	log(...args: any[]): void {
+		console.log(args);
+	}
+	async error(...args: any[]): Promise<void> {
+		console.error(args);
 	}
 }
 
-new User().addSkill('dwa');
+interface IPayable {
+	pay(paymentId: number): void;
+	price?: number;
+}
+
+interface IDeletable {
+	delete(): void;
+}
+
+class User implements IPayable, IDeletable {
+	pay(paymentId: number | string): void {}
+
+	delete(): void {}
+
+	price?: number | undefined;
+}
