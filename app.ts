4a3ccd1c5c8fb8f4f7dc81c237aa586adbae01f8
Nike <nkaz2003@gmail.com>
2022-05-07 14:07:27 +0300

    Construtor's description

--- app.ts
+++ app.ts
@@ -1,64 +1,16 @@
-interface IPayment {
-	sum: number;
-	from: number;
-	to: number;
+class User {
+	name: string;
+	age: number;
+
+	constructor();
+	constructor(name: string);
+	constructor(age: number);
+	constructor(ageOrName?: string | number) {
+		if (typeof ageOrName === 'string') this.name = ageOrName;
+		else if (typeof ageOrName === 'number') this.age = ageOrName;
+	}
 }
 
-enum EPaymentStatus {
-	success = 'success',
-	failed = 'failed',
-}
-
-interface IPaymentRequest extends IPayment {}
-
-interface IDataSuccess extends IPayment {
-	databaseId: number;
-}
-
-interface IDataFailed {
-	errorMessage: string;
-	errorCode: number;
-}
-
-interface IResponseSuccess {
-	status: EPaymentStatus.success;
-	data: IDataSuccess;
-}
-
-interface IResponseFailed {
-	status: EPaymentStatus.failed;
-	data: IDataFailed;
-}
-
-type TRes = IResponseSuccess | IResponseFailed;
-
-function isSuccessRes(res: TRes): res is IResponseSuccess {
-	return res.status === EPaymentStatus.success;
-}
-
-function getIdFromData(res: TRes): number | never {
-	if (isSuccessRes(res)) return res.data.databaseId;
-	throw new Error(res.data.errorMessage);
-}
-
-console.log(
-	getIdFromData({
-		status: EPaymentStatus.success,
-		data: {
-			databaseId: 10,
-			from: 1,
-			sum: 2,
-			to: 3,
-		},
-	})
-);
-
-console.log(
-	getIdFromData({
-		status: EPaymentStatus.failed,
-		data: {
-			errorCode: 404,
-			errorMessage: 'Not found',
-		},
-	})
-);
+const user = new User('Nike');
+const user2 = new User();
+const user3 = new User(10);
