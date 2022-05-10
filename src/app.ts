4560e50ee23d2baf5aafbef912f91665a2ea5f08
Nike <nkaz2003@gmail.com>
2022-05-10 11:21:49 +0300

    Generics + types/interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,30 @@
-interface IObject {
-	id: number;
-}
-function sortData<T extends IObject>(data: T[]): T[] {
-	return data.sort((a, b) => a.id - b.id);
-}
-const sorted = sortData([{ id: 2, name: 's' }, { id: 1 }]);
-console.log(sorted);
-
-function toString<T>(data: T): string {
-	switch (typeof data) {
-		case 'string':
-			return data;
-
-		case 'bigint':
-		case 'number':
-		case 'function':
-		case 'symbol':
-		case 'boolean':
-			return data.toString();
-
-		case 'object':
-			return JSON.stringify(data);
-
-		case 'undefined':
-		default:
-			return 'undefined';
-	}
+function logMiddleware<T>(data: T): T {
+	console.log(data);
+	return data;
 }
 
-console.log(toString([1, 2, 3, { a: 1 }]));
+// const res = logMiddleware<string>(10);
+const res = logMiddleware<number>(10);
+
+function getSplitedHalf<T>(arr: T[]): T[] {
+	const len = arr.length / 2;
+	return arr.splice(0, len);
+}
+
+const split1: <T>(arr: T[]) => T[] = getSplitedHalf;
+const split2: <SOME>(arr: SOME[]) => SOME[] = getSplitedHalf;
+
+interface ILogLine<T> {
+	timestamp: Date;
+	data: T;
+}
+
+type TLogline<T> = {
+	timestamp: Date;
+	data: T;
+};
+
+const logLine: ILogLine<{ a: number }> = {
+	timestamp: new Date(),
+	data: { a: 1 },
+};
