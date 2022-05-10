8f4c21490152d9b14425080620b9737c3dc67dfb
Nike <nkaz2003@gmail.com>
2022-05-10 13:03:12 +0300

    keyof

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,100 @@
-type TConstructor = new (...args: any[]) => {};
-type TGConstructor<T = {}> = new (...args: any[]) => T;
-
-class List {
-	items: string[];
+interface IUser {
+	name: string;
+	age: number;
 }
 
-class Accordion {
-	isOpened: boolean;
+// type KeysOfUser = keyof IUser;
+
+// const key: KeysOfUser = 'age';
+
+function getValue<T extends IUser, K extends keyof T>(obj: T, key: K) {
+	return obj[key];
 }
 
-// типы, которые определяют класс с установленными в интерфейс полями
-type TList = TGConstructor<List>;
-type TAccordion = TGConstructor<Accordion>;
+const user: IUser = {
+	name: 'Nike',
+	age: 18,
+};
 
-// class ExtendedListClass extends List {
-// 	first() {
-// 		return this.items[0];
-// 	}
-// }
+const userName = getValue(user, 'name');
 
-function ExtendedList<TBase extends TList & TAccordion>(Base: TBase) {
-	return class ExtendedList extends Base {
-		first() {
-			console.log(this.isOpened);
-			return this.items[0];
+//////////////
+
+function toString(data: unknown): string {
+	switch (typeof data) {
+		case 'string':
+			return data;
+
+		case 'bigint':
+		case 'number':
+		case 'boolean':
+		case 'symbol':
+		case 'function':
+			return data.toString();
+
+		case 'object':
+			return JSON.stringify(data);
+
+		case 'undefined':
+			return 'undefined';
+	}
+}
+
+const fromEntries = <T>(entries: Array<[unknown, T]>): { [key: string]: T } => {
+	const res: { [key: string]: T } = {};
+
+	entries.forEach(elem => (res[toString(elem[0])] = elem[1]));
+
+	return res;
+};
+
+interface IGroupByKey<ObjT> extends Record<string, ObjT[]> {}
+
+type key = string | number | symbol;
+
+function groupByKey<T extends Record<key, any>>(
+	data: T[],
+	key: keyof T
+): IGroupByKey<T> {
+	const res: IGroupByKey<T> = {};
+
+	let added = false;
+	for (const obj of data) {
+		for (const resKey in res) {
+			if (resKey === toString(obj[key])) {
+				res[resKey]?.push(obj);
+				added = true;
+				break;
+			}
 		}
-	};
+
+		if (!added) res[toString(obj[key])] = [obj];
+		else added = false;
+	}
+
+	return res;
 }
 
-class AccordionList {
-	isOpened: boolean;
-	constructor(public items: string[]) {}
+interface IUser2 extends IUser {
+	group: number;
 }
 
-const list = ExtendedList(AccordionList);
-const res = new list(['first', 'second']);
-console.log(res.first());
+const data: IUser2[] = [
+	{
+		group: 2,
+		name: 'Nike',
+		age: 18,
+	},
+	{
+		group: 2,
+		name: 'Olga',
+		age: 23,
+	},
+	{
+		group: 1,
+		name: 'Sergey',
+		age: 32,
+	},
+];
+
+console.log(groupByKey(data, 'group'));
