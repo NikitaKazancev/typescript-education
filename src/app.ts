ca32b44407e7db9056c38bd4e4d06257a39f2be5
Nike <nkaz2003@gmail.com>
2022-05-20 20:41:59 +0300

    Awaited

--- src/app.ts
+++ src/app.ts
@@ -1,73 +1,23 @@
-interface IUser {
+type A = Awaited<Promise<string>>;
+type A2 = Awaited<Promise<Promise<string>>>;
+
+interface IMenu {
 	name: string;
-	age?: number;
-	email: string;
+	url: string;
 }
 
-// Partial
-type partial = Partial<IUser>;
-
-type Partial2<T> = {
-	[key in keyof T]?: T[key];
-};
-type partial2 = Partial2<IUser>;
-
-const p: partial = {};
-
-// Required
-type required = Required<IUser>;
-
-type Required2<T> = {
-	[key in keyof T]-?: T[key];
-};
-type required2 = Required2<IUser>;
-
-const r: required = {
-	age: 1,
-	email: 'email',
-	name: 'Nike',
-};
-
-// Readonly
-const readOnly: Readonly<IUser> = {
-	email: 'email',
-	name: 'Nike',
-};
-
-////////////////////
-
-interface IPaymentPersistent {
-	id: number;
-	sum: number;
-	from: string;
-	to: string;
+async function getMenu(): Promise<IMenu[]> {
+	return [{ name: 'Analytics', url: 'analytics' }];
 }
 
-// Omit, Pick
-type Payment = Omit<IPaymentPersistent, 'id'>;
-type PaymentRequisites = Pick<IPaymentPersistent, 'from' | 'to'>;
+type R = Awaited<ReturnType<typeof getMenu>>;
 
-// Extract, Exclude
-type ExtractEx = Extract<'from' | 'to' | Payment, string>; // takes right types from union type
-type ExcludeEx = Exclude<'from' | 'to' | Payment, string>; // does not take right types from union type
-
-//////////////
-
-class User {
-	constructor(public id: number, public name: string) {}
+// more correct
+async function getArray<T>(x: T): Promise<Awaited<T>[]> {
+	return [await x];
 }
 
-function getData(id: number): User {
-	return new User(id, 'Nike');
+// less correct
+async function getArray2<T>(x: T): Promise<T[]> {
+	return [await x];
 }
-
-// Return/Parameter Type
-type RT = ReturnType<typeof getData>;
-type RT1 = ReturnType<() => void>;
-type RT2 = ReturnType<<T>() => T>; // unknown
-type RT3 = ReturnType<<T extends number>() => T>;
-
-type PTArray = Parameters<typeof getData>;
-type PT0 = Parameters<typeof getData>[0];
-
-type CP = ConstructorParameters<typeof User>;
