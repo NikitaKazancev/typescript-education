b6e13e4ac3bff2780ad9ca32fd04626e619d6697
Nike <nkaz2003@gmail.com>
2022-05-20 20:27:17 +0300

    Built-in additional types

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,73 @@
-type AnimationName = 'fade' | 'unFade';
-type AnimationFunction = 'in' | 'out' | 'inOut';
-
-type AnimationT =
-	`${Capitalize<AnimationName>}${Capitalize<AnimationFunction>}`;
-
-type ErrorOrSuccess = 'error' | 'success';
-interface IResponse {
-	result: `http${Capitalize<ErrorOrSuccess>}`;
+interface IUser {
+	name: string;
+	age?: number;
+	email: string;
 }
 
-const a: IResponse = {
-	result: 'httpError',
+// Partial
+type partial = Partial<IUser>;
+
+type Partial2<T> = {
+	[key in keyof T]?: T[key];
+};
+type partial2 = Partial2<IUser>;
+
+const p: partial = {};
+
+// Required
+type required = Required<IUser>;
+
+type Required2<T> = {
+	[key in keyof T]-?: T[key];
+};
+type required2 = Required2<IUser>;
+
+const r: required = {
+	age: 1,
+	email: 'email',
+	name: 'Nike',
 };
 
-// infer
-type Pull<T> = T extends `Fade${infer R}` ? R : never;
-type AnimationFunction2 = Pull<AnimationT>;
+// Readonly
+const readOnly: Readonly<IUser> = {
+	email: 'email',
+	name: 'Nike',
+};
+
+////////////////////
+
+interface IPaymentPersistent {
+	id: number;
+	sum: number;
+	from: string;
+	to: string;
+}
+
+// Omit, Pick
+type Payment = Omit<IPaymentPersistent, 'id'>;
+type PaymentRequisites = Pick<IPaymentPersistent, 'from' | 'to'>;
+
+// Extract, Exclude
+type ExtractEx = Extract<'from' | 'to' | Payment, string>; // takes right types from union type
+type ExcludeEx = Exclude<'from' | 'to' | Payment, string>; // does not take right types from union type
+
+//////////////
+
+class User {
+	constructor(public id: number, public name: string) {}
+}
+
+function getData(id: number): User {
+	return new User(id, 'Nike');
+}
+
+// Return/Parameter Type
+type RT = ReturnType<typeof getData>;
+type RT1 = ReturnType<() => void>;
+type RT2 = ReturnType<<T>() => T>; // unknown
+type RT3 = ReturnType<<T extends number>() => T>;
+
+type PTArray = Parameters<typeof getData>;
+type PT0 = Parameters<typeof getData>[0];
+
+type CP = ConstructorParameters<typeof User>;
