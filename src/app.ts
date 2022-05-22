11f81a4172011ea1c964b3efe5bac05181805787
Nike <nkaz2003@gmail.com>
2022-05-22 10:17:30 +0300

    get & set decorator

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,38 @@
 interface IUserService {
-	users: number;
 	getUsersInDB(): number;
 }
 
 class UserService implements IUserService {
-	@Max(100)
-	users: number = 1000;
+	private _users: number;
+
+	@Log()
+	set users(value: number) {
+		this._users = value;
+	}
+
+	get users() {
+		return this._users;
+	}
 
 	getUsersInDB(): number {
 		throw new Error('Error :(');
 	}
 }
 
-function Max(max: number) {
-	return (target: Object, propertyKey: string) => {
-		let value: number;
-
-		Object.defineProperty(target, propertyKey, {
-			set: (newValue: number) => {
-				if (newValue > max) {
-					console.info(
-						`## В ${propertyKey} было записано максимальное значение ${max} вместо ${newValue}`
-					);
-					value = max;
-				} else value = newValue;
-			},
-			get: () => value,
-		});
+function Log() {
+	return (
+		target: Object,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) => {
+		const set = descriptor.set;
+		descriptor.set = (...args: any) => {
+			console.log(args);
+			set?.apply(target, args);
+		};
 	};
 }
 
 const userService = new UserService();
 userService.users = 1;
-console.log(userService.users);
+console.log(userService);
