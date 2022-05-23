ba9f9968db543ca1a704e3a39e7af6153b8d1c41
Nike <nkaz2003@gmail.com>
2022-05-23 10:03:46 +0300

    Params decorators & metadata

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,81 @@
+import 'reflect-metadata';
+
+const POSITIVE_METADATA_KEY = Symbol('POSITIVE_METADATA_KEY');
+
 interface IUserService {
 	getUsersInDB(): number;
+	setUsersInDB(users: number, users2: number): void;
 }
 
 class UserService implements IUserService {
 	private _users: number;
 
-	@Log()
-	set users(value: number) {
-		this._users = value;
-	}
-
-	get users() {
-		return this._users;
+	@Validate()
+	setUsersInDB(@Positive() users: number, @Positive() users2: number): void {
+		this._users = users;
 	}
 
 	getUsersInDB(): number {
-		throw new Error('Error :(');
+		return this._users;
 	}
 }
 
-function Log() {
+function Positive() {
 	return (
 		target: Object,
-		propertyKey: string,
-		descriptor: PropertyDescriptor
+		propertyKey: string | symbol,
+		parameterIndex: number
 	) => {
-		const set = descriptor.set;
-		descriptor.set = (...args: any) => {
-			console.log(args);
-			set?.apply(target, args);
+		const existParams: number[] =
+			Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey) ||
+			[];
+		existParams.push(parameterIndex);
+		Reflect.defineMetadata(
+			POSITIVE_METADATA_KEY,
+			existParams,
+			target,
+			propertyKey
+		);
+	};
+}
+
+function Validate() {
+	return (
+		target: Object,
+		propertyKey: string | symbol,
+		descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+	) => {
+		const method = descriptor.value;
+		descriptor.value = (...args: any[]) => {
+			const positiveParams: number[] = Reflect.getOwnMetadata(
+				POSITIVE_METADATA_KEY,
+				target,
+				propertyKey
+			);
+
+			if (positiveParams) {
+				positiveParams.forEach(paramIndex => {
+					if (args[paramIndex] <= 0)
+						throw new Error(
+							`Parameters in method ${String(
+								propertyKey
+							)} must be positive`
+						);
+				});
+			}
+
+			return method?.apply(target, args);
 		};
 	};
 }
 
 const userService = new UserService();
-userService.users = 1;
-console.log(userService);
+
+try {
+	userService.setUsersInDB(10, -200);
+} catch (error) {
+	if (error instanceof Error) console.log(error.message);
+}
+
+userService.setUsersInDB(10, 200);
+console.log(userService.getUsersInDB());
