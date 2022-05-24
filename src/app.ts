cdc83bf3a64a04a805d3def033fb4d38433988ad
Nike <nkaz2003@gmail.com>
2022-05-24 21:29:11 +0300

    Decorators order

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,24 @@
-import 'reflect-metadata';
-
-const POSITIVE_METADATA_KEY = Symbol('POSITIVE_METADATA_KEY');
-
-interface IUserService {
-	getUsersInDB(): number;
-	setUsersInDB(users: number, users2: number): void;
-}
-
-class UserService implements IUserService {
-	private _users: number;
-
-	@Validate()
-	setUsersInDB(@Positive() users: number, @Positive() users2: number): void {
-		this._users = users;
-	}
-
-	getUsersInDB(): number {
-		return this._users;
-	}
-}
-
-function Positive() {
-	return (
-		target: Object,
-		propertyKey: string | symbol,
-		parameterIndex: number
-	) => {
-		const existParams: number[] =
-			Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey) ||
-			[];
-		existParams.push(parameterIndex);
-		Reflect.defineMetadata(
-			POSITIVE_METADATA_KEY,
-			existParams,
-			target,
-			propertyKey
-		);
+function Unic(message: string): any {
+	console.log(`Инициализация ${message}`);
+	return () => {
+		console.log(`Вызов ${message}`);
 	};
 }
 
-function Validate() {
-	return (
-		target: Object,
-		propertyKey: string | symbol,
-		descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
-	) => {
-		const method = descriptor.value;
-		descriptor.value = (...args: any[]) => {
-			const positiveParams: number[] = Reflect.getOwnMetadata(
-				POSITIVE_METADATA_KEY,
-				target,
-				propertyKey
-			);
+@Unic('Класс 1')
+@Unic('Класс 2')
+class Test {
+	@Unic('prop')
+	prop1: any;
 
-			if (positiveParams) {
-				positiveParams.forEach(paramIndex => {
-					if (args[paramIndex] <= 0)
-						throw new Error(
-							`Parameters in method ${String(
-								propertyKey
-							)} must be positive`
-						);
-				});
-			}
+	@Unic('static prop')
+	static prop2: any;
 
-			return method?.apply(target, args);
-		};
-	};
+	constructor(@Unic('constructor param') param: any) {}
+
+	@Unic('method')
+	method1(@Unic('method param') param: any) {}
+
+	@Unic('static method')
+	static method2(@Unic('static method param') param: any) {}
 }
-
-const userService = new UserService();
-
-try {
-	userService.setUsersInDB(10, -200);
-} catch (error) {
-	if (error instanceof Error) console.log(error.message);
-}
-
-userService.setUsersInDB(10, 200);
-console.log(userService.getUsersInDB());
