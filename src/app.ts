cb29ce1db37e0df5dc799750c3a2a3aa49777844
Nike <nkaz2003@gmail.com>
2022-05-29 12:09:29 +0300

    Principle of imports

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,3 @@
-function Unic(message: string): any {
-	console.log(`Инициализация ${message}`);
-	return () => {
-		console.log(`Вызов ${message}`);
-	};
-}
+import { a } from './modules/app2.js';
 
-@Unic('Класс 1')
-@Unic('Класс 2')
-class Test {
-	@Unic('prop')
-	prop1: any;
-
-	@Unic('static prop')
-	static prop2: any;
-
-	constructor(@Unic('constructor param') param: any) {}
-
-	@Unic('method')
-	method1(@Unic('method param') param: any) {}
-
-	@Unic('static method')
-	static method2(@Unic('static method param') param: any) {}
-}
+console.log(a);
---
+++ src/modules/app2.ts
@@ -0,0 +1,5 @@
+export const a = 5;
+
+export interface A {
+	coin: number;
+}
