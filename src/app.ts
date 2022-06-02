8a0b8cf36239e10437ba9f0916bea6d154b074d8
Nike <nkaz2003@gmail.com>
2022-06-02 20:19:55 +0300

    Bridge

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,47 @@
-enum ImageFormat {
-	PNG = 'png',
-	JPEG = 'jpeg',
+interface IProvider {
+	sendMessage(message: string): void;
+	connect(config: unknown): void;
+	disconnect(): void;
 }
 
-interface IResolution {
-	width: number;
-	height: number;
-}
-
-interface IImageConversion extends IResolution {
-	format: ImageFormat;
-}
-
-class ImageBuilder {
-	private formats: ImageFormat[] = [];
-	private resolutions: IResolution[] = [];
-
-	addPng(): this {
-		if (!this.formats.includes(ImageFormat.PNG))
-			this.formats.push(ImageFormat.PNG);
-
-		return this;
+class TelegramProvider implements IProvider {
+	sendMessage(message: string): void {
+		console.log(message);
 	}
-
-	addJpeg(): this {
-		if (!this.formats.includes(ImageFormat.JPEG))
-			this.formats.push(ImageFormat.JPEG);
-
-		return this;
+	connect(config: string): void {
+		console.log(config);
 	}
-
-	addResolution(width: number, height: number): this {
-		this.resolutions.push({ width, height });
-		return this;
-	}
-
-	build(): IImageConversion[] {
-		const res: IImageConversion[] = [];
-		this.resolutions.forEach(r =>
-			this.formats.forEach(f =>
-				res.push({
-					format: f,
-					height: r.height,
-					width: r.width,
-				})
-			)
-		);
-
-		return res;
+	disconnect(): void {
+		console.log('Disconnected telegram');
 	}
 }
 
-console.log(
-	new ImageBuilder()
-		.addJpeg()
-		.addPng()
-		.addResolution(100, 50)
-		.addResolution(200, 200)
-		.build()
-);
+class WhatsAppProvider implements IProvider {
+	sendMessage(message: string): void {
+		console.log(message);
+	}
+	connect(config: string): void {
+		console.log(config);
+	}
+	disconnect(): void {
+		console.log('Disconnected whatsapp');
+	}
+}
+
+class NotificationCenter {
+	constructor(private provider: IProvider) {}
+
+	send() {
+		this.provider.connect('connect');
+		this.provider.sendMessage('message');
+		this.provider.disconnect();
+	}
+}
+
+class DelayNotificationCenter extends NotificationCenter {
+	sendDelayed() {}
+}
+
+const sender = new DelayNotificationCenter(new TelegramProvider());
+sender.sendDelayed();
+sender.send();
